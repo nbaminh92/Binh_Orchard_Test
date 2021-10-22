c2598b9c66264819d9c6a1e99b2bc80e11ddc88e
Binh Anh Minh Ngo <nba.minh7892@gmail.com>
2021-10-22 22:08:00 +1100

    add Modal code to top section images
    Click on each image will display its modal form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 // Get element on click
 window.onclick = e => {
     console.log(e.target);  // to get the element
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    // use the target from Window onclick to get the ID of the image
+    var img = document.getElementById(e.target.id);
+
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    // Check if the tag name is img or else it might confuse with other element that has ID
+    if (e.target.tagName === 'IMG') {
+      img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      }
+    }
+
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
 }
